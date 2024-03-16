2b3c9f895f138ba048fbcc47c0018131c3c5b05f
zord04 <orozcorafael420@gmail.com>
2024-03-16 22:28:42 +0800

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+<script>
+  $(document).ready(function) () {
+    $('.carousel').carousel({
+      interval: 5000 // Change the duration in milliseconds (5000 ms = 5 seconds)
+    })
+  };
+
+</script>
